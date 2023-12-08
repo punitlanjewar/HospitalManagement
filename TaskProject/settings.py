935a1433a389f6064e42f479e3024193ecd4b9aa
punitlanjewar <pblanjewar22@gmail.com>
2023-12-08 17:31:28 +0530

    done vercel settings

--- TaskProject/settings.py
+++ TaskProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_7!d5xy*%vssa2x&snylpl3t!ccr7e!+fw9_xcol9nh(*(j6yt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -122,14 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_Root = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIR = {
-    os.path.join(BASE_DIR, "public/static")
-}
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
-MEDIA_URL = '/media/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
